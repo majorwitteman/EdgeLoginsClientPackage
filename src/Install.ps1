eebab6eb0bb5f678d10af89f2b4d79129f22af6a
Rob Witteman <rwitteman@pike.com>
2023-05-24 10:16:28 -0400

    reconfigure pipeline and Install.ps1 to protect function key

--- src/Install.ps1
+++ src/Install.ps1
@@ -1,9 +1,11 @@
+$functionKey = "<functionKey>" #secret in DevOps
+
 $DestinationPath = [System.IO.Path]::Combine($env:SystemRoot,"Temp","EdgeLoginsPackage")
 try {
     Copy-Item -Path $PSScriptRoot\Package -Destination $DestinationPath -Recurse -ErrorAction Stop -Force
     Write-Output "Copied package to $DestinationPath"
     Push-Location -Path $DestinationPath
-    .\Process.ps1 -FunctionUri "https://rwedgelogins.azurewebsites.net/api/receive?code=zyAhAQyuAiAbdy5ejtXIzTNenJ5SJBlNAqSVpgZR4GC_AzFujWvLjg=="
+    .\Process.ps1 -FunctionUri "https://rwedgelogins.azurewebsites.net/api/receive?code=$functionKey"
     Pop-Location
 }
 catch {
